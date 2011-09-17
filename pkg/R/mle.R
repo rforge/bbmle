46d41c67018c57ab9702e312b187d5fc6882495a
bbolker <bbolker@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-09-17 16:43:42 +0000

    change to evaluating all of call$control
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/bbmle@97 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/mle.R
+++ pkg/R/mle.R
@@ -206,9 +206,11 @@ mle2 <- function(minuslogl,
   call$data <- eval.parent(call$data)
   call$upper <- eval.parent(call$upper)
   call$lower <- eval.parent(call$lower)
-  call$control$parscale <- eval.parent(call$control$parscale)
-  call$control$ndeps <- eval.parent(call$control$ndeps)
-  call$control$maxit <- eval.parent(call$control$maxit)
+  ## FIX based on request from Mark Clements
+  ## call$control$parscale <- eval.parent(call$control$parscale)
+  ## call$control$ndeps <- eval.parent(call$control$ndeps)
+  ## call$control$maxit <- eval.parent(call$control$maxit)
+  call$control <- eval.parent(call$control)
   if(!missing(start))
     if (!is.list(start)) {
       if (is.null(names(start)) || !is.vector(start))
